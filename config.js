8280705bd68030cd589cf05ae444abce0de3d6d9
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-01-11 03:21:55 -0800

    added confs

---
+++ config.js
@@ -0,0 +1,30 @@
+/**
+ * Environment Variables script
+ */
+
+var environments = {};
+
+// staging (default) environment
+environments.staging = {
+    httpPort: 3000,
+    httpsPort: 3001,
+    envName: 'staging',
+    hashToken: 'thisIsAsecret'
+};
+
+// production environment
+environments.production = {
+    httpPort: 5000,
+    httpsPort: 5001,
+    envName: 'production',
+    hashToken: 'thisIsAnothersecret'
+};
+
+// determine which environent variable is passed as command line arg
+var currEnvironment = typeof(process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// check if the current environment is a valid one, else default to, staging environment
+var envToExport = typeof(environments[currEnvironment]) === 'object' ? environments[currEnvironment] : environments.staging;
+
+// export the active environment
+module.exports = envToExport;
