bb1aa50aa96fe81bb3e510fea0488ecf1510bf92
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-01-11 02:48:35 -0800

    server setup

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello World')
+/**
+ * API Configuration script
+ */
+
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+var httpServer = http.createServer(function(req, res) {
+    res.end('Hello world');
+});
+
+httpServer.listen(3000, function() {
+    console.log('Server running on port port 3000');
+});
+
+
