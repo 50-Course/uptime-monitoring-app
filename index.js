42742a177a14159717c9a26c76c3c45ab92c1d1f
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-01-13 05:54:06 -0800

    feat: tokens service completed

--- index.js
+++ index.js
@@ -107,5 +107,6 @@ var unifiedServer = function(req, res) {
 var router = {
     sample: handlers.sample,
     ping: handlers.ping,
-    users: handlers.users
+    users: handlers.users,
+    tokens: handlers.tokens
 };
